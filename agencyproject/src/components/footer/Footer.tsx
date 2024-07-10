68e7332552a9ab9da2c98bc646a227a0c793d8cb
Malshani RathnaSri <malshanirathnasri@gmail.com>
2024-07-11 01:11:14 +0530

    footer

--- agencyproject/src/components/footer/Footer.tsx
+++ agencyproject/src/components/footer/Footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.container}>
+      <div className={styles.logo}>Lama Agency</div>
+        <div>
+          Lama creative throughts agency &copy; All rights reserved.
+        </div>
     </div>
   )
 }
